6d4e1b4ab6444f94ecae32adaf5c62185e6bea07
BI4PYM <1263188827@qq.com>
2024-12-05 01:38:00 +0800

    Update AutoTLE.py

--- AutoTLE.py
+++ AutoTLE.py
@@ -8,10 +8,12 @@ output2 = '\n' + subprocess.run("python main.py", shell=True, capture_output=Tru
 print(output2)
 output3 = '\n' + subprocess.run("git add *", shell=True, capture_output=True, text=True).stdout
 print(output3)
-output4 = '\n' + subprocess.run("git commit -a -m 'Auto upload'", shell=True, capture_output=True, text=True).stdout
+output4 = '\n' + subprocess.run("git add logs.txt'", shell=True, capture_output=True, text=True).stdout
 print(output4)
-output5 = '\n' + subprocess.run("git push -u --force origin master", shell=True, capture_output=True, text=True).stdout
+output5 = '\n' + subprocess.run("git commit -a -m 'Auto upload'", shell=True, capture_output=True, text=True).stdout
 print(output5)
-output = output + output1 + output2 + output3 + output4 + output5
+output6 = '\n' + subprocess.run("git push -u --force origin master", shell=True, capture_output=True, text=True).stdout
+print(output6)
+output = output + output1 + output2 + output3 + output4 + output5 + output6
 with open("logs.txt", "w") as file:
     file.write(output)
