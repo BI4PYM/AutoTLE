dd31fc8b2fd09fd9baed74c1b0d4bdca57ea804b
BI4PYM <1263188827@qq.com>
2024-12-05 01:29:36 +0800

    Create AutoTLE.py

---
+++ AutoTLE.py
@@ -0,0 +1,17 @@
+import subprocess
+
+output = subprocess.run("date /t   && time /t", shell=True, capture_output=True, text=True).stdout
+print(output)
+output1 = '\n' + subprocess.run("git pull origin master", shell=True, capture_output=True, text=True).stdout
+print(output1)
+output2 = '\n' + subprocess.run("python main.py", shell=True, capture_output=True, text=True).stdout
+print(output2)
+output3 = '\n' + subprocess.run("git add *", shell=True, capture_output=True, text=True).stdout
+print(output3)
+output4 = '\n' + subprocess.run("git commit -a -m 'Auto upload'", shell=True, capture_output=True, text=True).stdout
+print(output4)
+output5 = '\n' + subprocess.run("git push -u --force origin master", shell=True, capture_output=True, text=True).stdout
+print(output5)
+output = output + output1 + output2 + output3 + output4 + output5
+with open("logs.txt", "w") as file:
+    file.write(output)
